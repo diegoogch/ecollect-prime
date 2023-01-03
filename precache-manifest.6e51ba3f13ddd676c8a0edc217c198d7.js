4c23b8e44b75e1fb455d0e593fad3b16e8d533d7
diegoor <diegoggonzales7@gmail.com>
2023-01-03 10:25:37 -0500

    Updates

---
+++ precache-manifest.6e51ba3f13ddd676c8a0edc217c198d7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6ae4c5b8fb3e90c7a6307b5ec89a10",
+    "url": "/ecollect-prime/index.html"
+  },
+  {
+    "revision": "0924819fa2eb51ed94d9",
+    "url": "/ecollect-prime/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "3d7b3001b076784f4e03",
+    "url": "/ecollect-prime/static/css/main.b9c091fb.chunk.css"
+  },
+  {
+    "revision": "0924819fa2eb51ed94d9",
+    "url": "/ecollect-prime/static/js/2.d4dff009.chunk.js"
+  },
+  {
+    "revision": "3d7b3001b076784f4e03",
+    "url": "/ecollect-prime/static/js/main.56302da5.chunk.js"
+  },
+  {
+    "revision": "aa0c6ac264b07a0181cf",
+    "url": "/ecollect-prime/static/js/runtime~main.530b9949.js"
+  },
+  {
+    "revision": "ff1903b2f72fb064f261806a06c56a47",
+    "url": "/ecollect-prime/static/media/calidad.ff1903b2.png"
+  },
+  {
+    "revision": "0da104b4ab43cc3503cc934a1adfd645",
+    "url": "/ecollect-prime/static/media/carrusel1.0da104b4.png"
+  },
+  {
+    "revision": "3945b2f9eb23af694e704f086c869aab",
+    "url": "/ecollect-prime/static/media/carrusel2.3945b2f9.png"
+  },
+  {
+    "revision": "a950df9b73830daecdab23d459bc7e6f",
+    "url": "/ecollect-prime/static/media/carrusel3.a950df9b.png"
+  },
+  {
+    "revision": "c3f9dcb7d0cbc241da6200b7bb44761c",
+    "url": "/ecollect-prime/static/media/comentarioJ.c3f9dcb7.PNG"
+  },
+  {
+    "revision": "0b3757a0e5269a323102694d78d61593",
+    "url": "/ecollect-prime/static/media/comentarioL.0b3757a0.PNG"
+  },
+  {
+    "revision": "91c32d2af906885c8d698a2a700e7f09",
+    "url": "/ecollect-prime/static/media/comentarioM.91c32d2a.PNG"
+  },
+  {
+    "revision": "7df30611ca44dc45cb9f741201023667",
+    "url": "/ecollect-prime/static/media/finalizado.7df30611.png"
+  },
+  {
+    "revision": "b2e449f39c553f89cd4465be80f5f593",
+    "url": "/ecollect-prime/static/media/logotipo_original.b2e449f3.png"
+  },
+  {
+    "revision": "cf612db8276f71d2ae0fa8adb01bb5b3",
+    "url": "/ecollect-prime/static/media/reciclaje.cf612db8.png"
+  },
+  {
+    "revision": "6566d5d680ae24332cf63c40cee16a9c",
+    "url": "/ecollect-prime/static/media/registro.6566d5d6.png"
+  },
+  {
+    "revision": "07b34fdfafa1b3f6324b461a73828631",
+    "url": "/ecollect-prime/static/media/seguridad.07b34fdf.png"
+  }
+]);
